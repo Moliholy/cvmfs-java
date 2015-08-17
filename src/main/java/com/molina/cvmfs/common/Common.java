3a779c07174fed2d333b75b2ac4cd226726cc09d
Jose Molina <gaudy41@gmail.com>
2015-08-17 23:37:40 +0200

    Added binaryBufferToHexString method to Common class

--- src/main/java/com/molina/cvmfs/common/Common.java
+++ src/main/java/com/molina/cvmfs/common/Common.java
@@ -13,4 +13,11 @@ public class Common {
     public static final String MANIFEST_NAME = ".cvmfspublished";
     public static final String LAST_REPLICATION_NAME = ".cvmfs_last_snapshot";
     public static final String REPLICATING_NAME = ".cvmfs_is_snapshotting";
+
+    public static String binaryBufferToHexString(byte[] binaryBuffer) {
+        StringBuilder sb = new StringBuilder();
+        for (byte b : binaryBuffer)
+            sb.append(String.format("%02X ", b));
+        return sb.toString().toLowerCase();
+    }
 }
