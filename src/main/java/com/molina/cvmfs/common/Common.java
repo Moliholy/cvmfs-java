300510d0ecf2d2343376f9bb7ff4188f34cd7582
Jose Molina <gaudy41@gmail.com>
2015-08-17 20:06:13 +0200

    Added constant Strings under the Common class

---
+++ src/main/java/com/molina/cvmfs/common/Common.java
@@ -0,0 +1,16 @@
+package com.molina.cvmfs.common;
+
+/**
+ * @author Jose Molina Colmenero
+ */
+public class Common {
+
+    public static final String REPO_CONFIG_PATH = "/etc/cvmfs/repositories.d";
+    public static final String SERVER_CONFIG_NAME = "server.conf";
+
+    public static final String REST_CONNECTOR = "control";
+
+    public static final String MANIFEST_NAME = ".cvmfspublished";
+    public static final String LAST_REPLICATION_NAME = ".cvmfs_last_snapshot";
+    public static final String REPLICATING_NAME = ".cvmfs_is_snapshotting";
+}
