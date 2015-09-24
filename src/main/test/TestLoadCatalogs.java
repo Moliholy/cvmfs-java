11cbd420ebfc4108817bab88d1bd9a2f5db7b284
Jose Molina <jose.molina@cern.ch>
2015-09-24 14:42:57 +0200

    Added TestLoadCatalogs class

---
+++ src/main/test/TestLoadCatalogs.java
@@ -0,0 +1,30 @@
+import com.molina.cvmfs.catalog.Catalog;
+import com.molina.cvmfs.catalog.CatalogReference;
+import com.molina.cvmfs.repository.Repository;
+import com.molina.cvmfs.repository.exception.CacheDirectoryNotFound;
+import com.molina.cvmfs.repository.exception.FailedToLoadSourceException;
+import com.molina.cvmfs.rootfile.exception.RootFileException;
+
+import java.io.IOException;
+
+public class TestLoadCatalogs {
+
+    private static Repository repo;
+
+    public static void main(String[] args)
+            throws RootFileException, CacheDirectoryNotFound, FailedToLoadSourceException, IOException {
+        repo = new Repository("http://cvmfs-stratum-one.cern.ch/opt/boss");
+        Catalog rootCatalog = repo.retrieveRootCatalog();
+        System.out.println("Downloading the root catalog");
+        retrieveCatalogTree(rootCatalog);
+    }
+
+    private static void retrieveCatalogTree(Catalog catalog) {
+        CatalogReference[] refs = catalog.listNested();
+        for (CatalogReference ref : refs) {
+            System.out.println("Downloading the catalog in " + ref.getRootPath());
+            Catalog newCatalog = ref.retrieveFrom(repo);
+            retrieveCatalogTree(newCatalog);
+        }
+    }
+}
