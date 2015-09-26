38e70a5cc8f963d7592eee4f09fff0d674288ec3
Jose Molina <jose.molina@cern.ch>
2015-09-26 22:10:26 +0200

    Modified buffer size in Fetcher

--- src/main/java/com/molina/cvmfs/repository/fetcher/Fetcher.java
+++ src/main/java/com/molina/cvmfs/repository/fetcher/Fetcher.java
@@ -12,7 +12,7 @@ import java.util.zip.InflaterInputStream;
  * @author Jose Molina Colmenero
  */
 public class Fetcher {
-    private static final int FETCHER_BUFFER_SIZE = 10 * 1024 * 1024;  // 10MB
+    private static final int FETCHER_BUFFER_SIZE = 2 * 1024 * 1024;  // 2MB
 
     protected Cache cache;
     protected URL source;
