8c5e650153afea4edaaad1970ae464f01c2a1135
Jose Molina <jmolinacolmenero@gmail.com>
2015-09-29 01:49:20 +0200

    Added constant

--- src/main/java/com/molina/cvmfs/common/Common.java
+++ src/main/java/com/molina/cvmfs/common/Common.java
@@ -15,7 +15,7 @@ public class Common {
     public static final String SERVER_CONFIG_NAME = "server.conf";
 
     public static final String REST_CONNECTOR = "control";
-
+    public static final String WHITELIST_NAME = ".cvmfswhitelist";
     public static final String MANIFEST_NAME = ".cvmfspublished";
     public static final String LAST_REPLICATION_NAME = ".cvmfs_last_snapshot";
     public static final String REPLICATING_NAME = ".cvmfs_is_snapshotting";
