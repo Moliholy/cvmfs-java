8529ca49a5d9b068ba72f707655d284e8257bb84
Jose Molina <jose.molina@cern.ch>
2015-11-26 12:21:00 +0100

    Rename exception

--- src/main/java/com/molina/cvmfs/repository/exception/RepositoryNotFoundException.java
+++ src/main/java/com/molina/cvmfs/repository/exception/RepositoryNotFoundException.java
@@ -3,9 +3,9 @@ package com.molina.cvmfs.repository.exception;
 /**
  * @author Jose Molina Colmenero
  */
-public class RepositoryNotFound extends Exception {
+public class RepositoryNotFoundException extends Exception {
 
-    public RepositoryNotFound(String repositoryURI) {
+    public RepositoryNotFoundException(String repositoryURI) {
         super(repositoryURI + " not found");
     }
 }
